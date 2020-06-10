223a20c13803ffead31d87ae6a3f5a542a95279c
Marek Miklaszewski <mar.mik1@wp.pl>
2020-06-10 22:19:28 +0200

    Updates

---
+++ precache-manifest.7bfb3b333c454e959c5ab27c41461aca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "320cbc247be48a831319c79be34cf8d6",
+    "url": "/covid/index.html"
+  },
+  {
+    "revision": "f21561863b74cf729904",
+    "url": "/covid/static/js/2.22d7d13b.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/covid/static/js/2.22d7d13b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f502bf7b152362534a4",
+    "url": "/covid/static/js/main.67b88e90.chunk.js"
+  },
+  {
+    "revision": "ce8b68cfde1d2241b2f5",
+    "url": "/covid/static/js/runtime-main.6cf0ecb8.js"
+  }
+]);
